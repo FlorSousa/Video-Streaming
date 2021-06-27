e271244f456f0d56ae40dbf84bd8baaf1611eaa2
Jhonatas Flor de Sousa <58887452+FlorSousa@users.noreply.github.com>
2021-06-27 20:37:55 -0300

    Sem mais autenticação

--- service-django-neo4j/login/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-#subrotas
-urlpatterns = [ 
-    path('', views.index)
-]
--- service-django-neo4j/login/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.http import HttpRequest
-
-# Create your views here.
-def index(request):
-    pass
